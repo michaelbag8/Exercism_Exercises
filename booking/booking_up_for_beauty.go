45551cdb7adb6ba16a968e580b4ababbf3471304
michaelbag8 <michaelbag8@gmail.com>
2026-05-29 15:33:44 +0100

    Added booking

---
+++ booking/booking_up_for_beauty.go
@@ -0,0 +1,57 @@
+package booking
+
+import (
+    "time"
+    "fmt"
+)
+
+// Schedule returns a time.Time from a string containing a date.
+func Schedule(date string) time.Time {
+	layout := "1/2/2006 15:04:05"
+	t, _ := time.Parse(layout, date)
+	return t
+}
+
+// HasPassed returns whether a date has passed.
+func HasPassed(date string) bool {
+	layout := "January 2, 2006 15:04:05"
+	t, _ := time.Parse(layout, date)
+
+	return t.Before(time.Now())
+}
+
+// IsAfternoonAppointment returns whether a time is in the afternoon.
+func IsAfternoonAppointment(date string) bool {
+	layout := "Monday, January 2, 2006 15:04:05"
+	t, _ := time.Parse(layout, date)
+
+	hour := t.Hour()
+
+	return hour >= 12 && hour < 18
+}
+
+// Description returns a formatted string of the appointment time.
+func Description(date string) string {
+	t := Schedule(date)
+
+	return fmt.Sprintf(
+		"You have an appointment on %s.",
+		t.Format("Monday, January 2, 2006, at 15:04"),
+	)
+}
+
+// AnniversaryDate returns a Time with this year's anniversary.
+func AnniversaryDate() time.Time {
+	currentYear := time.Now().Year()
+
+	return time.Date(
+		currentYear,
+		time.September,
+		15,
+		0,
+		0,
+		0,
+		0,
+		time.UTC,
+	)
+}
