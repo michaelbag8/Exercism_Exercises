58d4b473603830f1ae10ba30f2f9d3697fb8a4af
michaelbag8 <michaelbag8@gmail.com>
2026-05-30 15:07:37 +0100

    code formatted

--- gross/gross_store.go
+++ gross/gross_store.go
@@ -2,20 +2,20 @@ package gross
 
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
-    unit := map[string]int{
-       "quarter_of_a_dozen": 3, 
-        "half_of_a_dozen": 6,
-        "dozen": 12,
-         "small_gross": 120,
-        "gross": 144,
-         "great_gross": 1728,
-    }
+	unit := map[string]int{
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728,
+	}
 	return unit
 }
 
 // NewBill creates a new bill.
 func NewBill() map[string]int {
-    bill := make(map[string]int)
+	bill := make(map[string]int)
 	return bill
 }
 
@@ -32,40 +32,37 @@ func AddItem(bill, units map[string]int, item, unit string) bool {
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-   
-    _, ok := bill[item]
-    if !ok {
-        return false
-    }
 
-   
-    qty, ok := units[unit]
-    if !ok {
-        return false
-    }
+	_, ok := bill[item]
+	if !ok {
+		return false
+	}
 
-    newQty := bill[item] - qty
+	qty, ok := units[unit]
+	if !ok {
+		return false
+	}
 
-    
-    if newQty < 0 {
-        return false
-    }
+	newQty := bill[item] - qty
 
-   
-    if newQty == 0 {
-        delete(bill, item)
-    } else {
-        bill[item] = newQty
-    }
+	if newQty < 0 {
+		return false
+	}
 
-    return true
+	if newQty == 0 {
+		delete(bill, item)
+	} else {
+		bill[item] = newQty
+	}
+
+	return true
 }
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.
 func GetItem(bill map[string]int, item string) (int, bool) {
-    qty, ok := bill[item]
-    if !ok{
-        return 0, false
-    }
-    return qty, true
+	qty, ok := bill[item]
+	if !ok {
+		return 0, false
+	}
+	return qty, true
 }
