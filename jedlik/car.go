196d5198f4473d189ae0f1461b539413dae191f7
michaelbag8 <michaelbag8@gmail.com>
2026-06-02 21:51:22 +0100

    car.go added

--- jedlik/car.go
+++ jedlik/car.go
@@ -1,19 +1,26 @@
 package jedlik
-
-// Car implements a remote controlled car.
+// 1. Added the 'distance' and 'battery' fields to track the car's state.
 type Car struct {
 	speed        int
 	batteryDrain int
-
-	battery  int
-	distance int
+	battery      int
+	distance     int // Tracks total meters driven
 }
 
-// NewCar creates a new car with given specifications.
-func NewCar(speed, batteryDrain int) *Car {
-	return &Car{
+// NewCar creates a new car with a full 100% battery.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{
 		speed:        speed,
 		batteryDrain: batteryDrain,
 		battery:      100,
+		distance:     0,
+	}
+}
+
+// 2. Method with a pointer receiver (*Car) to update the fields directly.
+func (c *Car) Drive() {
+	if c.battery >= c.batteryDrain {
+		c.distance += c.speed
+		c.battery -= c.batteryDrain
 	}
 }
